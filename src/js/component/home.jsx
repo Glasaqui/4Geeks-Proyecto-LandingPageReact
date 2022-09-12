4a7507e08fe0f53c35a3da87c04672d35a673cbf
Glasaqui <gsanchezquiroz@outlook.com>
2022-09-12 04:30:31 +0000

    LandingPage

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,129 @@
 import React from "react";
+import reactDom from "react-dom";
+import propTypes from "prop-types";
+import {CardGroup,Card,Button,CardImg,CardBody,CardTitle,CardSubtitle,CardText } from 'reactstrap';
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Jumbotron = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
+		<div>
+		<div className="jumbotron">
+			<h1 className="display-4">A Warm Welcome!</h1>
+			<p className="lead">Loren ipsum dolor, sit amet consectetur adispicing eit. It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
+			<hr className="my-4"/>
+			<p className="lead">
+				<a className="btn btn-primary btn-lg" href="#" role="button">Call to actions</a>
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		</div>
+		
+		<CardGroup>
+		<Card>
+			<CardImg
+			alt="Card image cap"
+			src="https://picsum.photos/318/180"
+			top
+			width="100%"
+			/>
+			<CardBody>
+			<CardTitle tag="h5">
+				Card title
+			</CardTitle>
+			<CardSubtitle
+				className="mb-2 text-muted"
+				tag="h6"
+			>
+				Card subtitle
+			</CardSubtitle>
+			<CardText>
+				This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+			</CardText>
+			<Button>
+				Find out More!
+			</Button>
+			</CardBody>
+		</Card>
+		<Card>
+			<CardImg
+			alt="Card image cap"
+			src="https://picsum.photos/318/180"
+			top
+			width="100%"
+			/>
+			<CardBody>
+			<CardTitle tag="h5">
+				Card title
+			</CardTitle>
+			<CardSubtitle
+				className="mb-2 text-muted"
+				tag="h6"
+			>
+				Card subtitle
+			</CardSubtitle>
+			<CardText>
+				This card has supporting text below as a natural lead-in to additional content.
+			</CardText>
+			<Button>
+			Find out More!
+			</Button>
+			</CardBody>
+		</Card>
+		<Card>
+			<CardImg
+			alt="Card image cap"
+			src="https://picsum.photos/318/181"
+			top
+			width="100%"
+			/>
+			<CardBody>
+			<CardTitle tag="h5">
+				Card title
+			</CardTitle>
+			<CardSubtitle
+				className="mb-2 text-muted"
+				tag="h6"
+			>
+				Card subtitle
+			</CardSubtitle>
+			<CardText>
+				This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
+			</CardText>
+			<Button>
+			Find out More!
+			</Button>
+			</CardBody>
+		</Card>
+		<Card>
+			<CardImg
+			alt="Card image cap"
+			src="https://picsum.photos/318/181"
+			top
+			width="100%"
+			/>
+			<CardBody>
+			<CardTitle tag="h5">
+				Card title
+			</CardTitle>
+			<CardSubtitle
+				className="mb-2 text-muted"
+				tag="h6"
+			>
+				Card subtitle
+			</CardSubtitle>
+			<CardText>
+				This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
+			</CardText>
+			<Button>
+			Find out More!
+			</Button>
+			</CardBody>
+		</Card>
+
+		</CardGroup>
 		</div>
 	);
 };
 
-export default Home;
+export default Jumbotron;
